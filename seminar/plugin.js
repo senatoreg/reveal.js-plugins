a6d0c89f79778b91e61502493fcc434ec86e397c
Giovanni Senatore <giovanni.senatore@gmail.com>
2021-10-15 00:08:12 +0200

    Fix socket.io API

--- seminar/plugin.js
+++ seminar/plugin.js
@@ -51,7 +51,7 @@ const initSeminar = function(Reveal){
 
 	const logger = seminar.logger || defaultLogger;
 
-	var socket = io.connect( seminar.server, { path: seminar.server_path } );
+	var socket = io( seminar.server, { path: seminar.server_path } );
 	if ( seminar.callback )
 	logger('connect to ', seminar.server);
 	socket.on('connect', function() {
