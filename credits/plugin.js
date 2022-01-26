135cd032a34a11428cb0e906af09ebd702ec75bb
Giovanni Senatore <giovanni.senatore@gmail.com>
2022-01-26 09:30:05 +0100

    Semplification and parametrization of credits plugin

--- credits/plugin.js
+++ credits/plugin.js
@@ -16,40 +16,47 @@ window.RevealCredits = window.RevealCredits || {
 };
 
 const initCredits = function(Reveal){
-	var config = Reveal.getConfig().credits || {};
-	var width = Reveal.getConfig().width;
-	var height = Reveal.getConfig().height;
+	let config = Reveal.getConfig().credits || {};
+	let prefix = config.prefix;
+	let margin = config.margin;
 
-	var div = document.createElement( 'div' );
+	let div = document.createElement( 'div' );
 	div.id = 'credits';
 	div.classList.add( 'credits' );
+	div.hidden = true;
+	console.debug(config);
+	if (prefix)
+		div.style.setProperty('--r-credits-prefix', "'" + prefix + ":'");
+	if (margin)
+		div.style.setProperty('--r-credits-margin', margin);
 
-	var credit = document.createElement( 'div' );
-	credit.id = 'credit';
-	credit.classList.add( 'credit' );
-	div.appendChild(credit);
-
-	document.querySelectorAll('.reveal > div#credits > div#credit > *').forEach(el => { el.remove(); });
+	document.querySelectorAll('.reveal > div#credits > *').forEach(el => { el.remove(); });
 	document.querySelector('.reveal').appendChild( div );
 
 	Reveal.addEventListener('slidechanged', function( event ) {
 		event.previousSlide.querySelectorAll('sup.apex').forEach(el => { el.remove(); });
 
-		document.querySelectorAll('.reveal > div#credits > div#credit > *').forEach(el => { el.remove(); });
+		document.querySelectorAll('.reveal > div#credits > *').forEach(el => { el.remove(); });
 
-		let credit = document.querySelector('.reveal > div#credits > div#credit');
+		let credit = document.querySelector('.reveal > div#credits');
 		let infoCredit = event.currentSlide.getAttribute('data-info-credit');
 
 		if (infoCredit === undefined || infoCredit === null || infoCredit.length === 0) {
-			//credit.setAttribute('hidden', '');
 			return;
 		}
 
 		let p = document.createElement('p');
 		let text = document.createTextNode( infoCredit );
 		p.appendChild(text);
-		credit.appendChild(p);
-		//credit.removeAttribute('hidden');
+		div.appendChild(p);
+	});
+
+	Reveal.addEventListener('overviewshown', function( event ) {
+		div.style.setProperty('display', 'none');
+	});
+
+	Reveal.addEventListener('overviewhidden', function( event ) {
+		div.style.removeProperty('display');
 	});
 
 	return this;
