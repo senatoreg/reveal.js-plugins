0058c0a1535bf112ba14c9744f55ea73baaa843c
Giovanni Senatore <giovanni.senatore@gmail.com>
2023-11-19 15:57:12 +0100

    Switch from slidechanged event to beforeslidechange event

--- fittext/plugin.js
+++ fittext/plugin.js
@@ -100,8 +100,14 @@ const initFitText = function(Reveal){
 		fittext( Reveal.getCurrentSlide(), selectorFragment );
 	});
 
+	/*
 	Reveal.addEventListener('slidechanged', function( event ) {
 		fittext( event.currentSlide, selectorSlide );
+	});
+	*/
+
+	Reveal.addEventListener('beforeslidechange', function( event ) {
+		fittext( Reveal.getSlide( event.indexh, event.indexv ), selectorSlide );
 	});
 
 	Reveal.addEventListener('slidetransitionend', function( event ) {
